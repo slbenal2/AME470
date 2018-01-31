7c2dcef74582b353a8d832409c1965ee2b7240b8
student <student@dc-09.mobile.asu.edu>
2018-01-31 11:55:54 -0700

    sub,div,mult

--- staticServer.js
+++ staticServer.js
@@ -18,7 +18,22 @@ app.get("/add", function (req, res) {
 });
 
 app.get("/sub", function (req, res) {
-    res.send(req.query);
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a - b;
+    res.send(result.toString());
+});
+app.get("/div", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a / b;
+    res.send(result.toString());
+});
+app.get("/mult", function (req, res) {
+    var a = parseFloat(req.query.a);
+    var b = parseFloat(req.query.b);
+    var result = a * b;
+    res.send(result.toString());
 });
 
 app.use(methodOverride());
